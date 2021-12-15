1e0ab943de9f03ebd87a91a0f3514b594f300682
Markvitim <markvitim@gmail.com>
2021-12-15 12:28:16 +0300

    pom and = added

--- src/main/java/ru/netology/squareCounter/SquareCounterService.java
+++ src/main/java/ru/netology/squareCounter/SquareCounterService.java
@@ -5,7 +5,7 @@ public class SquareCounterService {
         int counter;
         counter = 0;
         for (int i = 10; i <= 99; i++) {
-            if (i * i < upperValue && i * i > lowValue) {
+            if (i * i <= upperValue && i * i >= lowValue) {
                 counter += 1;
             }
         }
